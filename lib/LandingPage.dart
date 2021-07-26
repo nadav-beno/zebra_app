e32d2538040fbac32896274ac477bd5c5b610926
Nadav Benoudiz <benoudiz15@gmail.com>
2021-07-26 15:39:45 +0300

    Update

---
+++ lib/LandingPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text('Drucker hha-hnon'),
+        centerTitle: true,
+
+      ),
+    );
+  }
+}
